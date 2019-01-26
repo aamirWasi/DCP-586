758f74317ac53badff58f357eaf961d22d9d5b2d
aamir <wasi3aamir@gmail.com>
2019-01-26 22:10:02 +0600

    Accepted

---
+++ DCP-586/DCP-586/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCP_586
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for(int t = 0; t < T; t++)
+            {
+                string line = Console.ReadLine();
+                string[] space = line.Split(' ');
+                int BronzeMedalCount = int.Parse(space[0]);
+                int SilverMedalCount = int.Parse(space[1]);
+                int GoldMedalCount = int.Parse(space[2]);
+
+                int totalMedals = BronzeMedalCount * 10 + SilverMedalCount * 50 + GoldMedalCount * 100;
+                Console.WriteLine($"{totalMedals}");
+            }
+        }
+    }
+}
